b83f43f5b1a5974bf3f9130a35d32539312e0d15
Bartek Skorulski <bartekskorulski@gmail.com>
2017-04-06 00:07:23 +0200

    adds input and add nodes

--- miniflow/node.py
+++ miniflow/node.py
@@ -25,3 +25,32 @@ class Node:
         for in_node in self.inbound_nodes:
             out  += str(in_node)
         return out + '*'
+
+
+class Input(Node):
+    def __init__(self):
+        # An Input node has no inbound nodes,
+        # so no need to pass anything to the Node instantiator.
+        Node.__init__(self)
+
+    # NOTE: Input node is the only node where the value
+    # may be passed as an argument to forward().
+    #
+    # All other node implementations should get the value
+    # of the previous node from self.inbound_nodes
+    #
+    # Example:
+    # val0 = self.inbound_nodes[0].value
+    def forward(self, value=None):
+        # Overwrite the value if one is passed in.
+        if value is not None:
+            self.value = value
+
+class Add(Node):
+    def __init__(self, x, y):
+        Node.__init__(self, [x, y])
+
+    def forward(self):
+        """
+        You'll be writing code here in the next quiz!
+        """
